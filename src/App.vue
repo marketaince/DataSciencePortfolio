3aeccec443487de839a790f9bcb3730dfcf7fc03
Markéta Ince <marketa.stepanova1@gmail.com>
2020-07-30 00:39:06 +0200

    all tabs allowed

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@
         <ul>
           <li><router-link to="/">Home</router-link></li>
           <li><router-link to="/about">About</router-link></li>
-<!--          <li><router-link to="/blog">Blog</router-link></li>-->
-<!--          <li><router-link to="/projects">Projects</router-link></li>-->
-<!--          <li><router-link to="/contact">Contact</router-link></li>-->
+          <li><router-link to="/blog">Blog</router-link></li>
+          <li><router-link to="/projects">Projects</router-link></li>
+          <li><router-link to="/contact">Contact</router-link></li>
         </ul>
       </div>
     </nav>
