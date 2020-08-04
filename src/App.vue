01da7f13dcf5cf6562c3f13a5372c4242bed5f35
Markéta Ince <marketa.stepanova1@gmail.com>
2020-08-05 01:54:05 +0200

    Rebased onto master

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
           <router-link to="/">Home</router-link>
           <router-link to="/about">About</router-link>
 <!--          <router-link to="/blog">Blog</router-link>-->
-          <router-link to="/projects">Projects</router-link>
+<!--          <router-link to="/projects">Projects</router-link>-->
           <router-link to="/contact">Contact</router-link>
         </ul>
       </div>
